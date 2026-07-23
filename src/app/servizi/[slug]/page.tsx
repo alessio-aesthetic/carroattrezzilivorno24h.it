027ee935b8d080f88695b0cf4770a55323a4c0d9
alessio-aesthetic <alessi.ola.rocchia@gmail.com>
2026-07-23 19:06:53 +0200

    Create Livorno roadside assistance site

--- src/app/servizi/[slug]/page.tsx
+++ src/app/servizi/[slug]/page.tsx
@@ -136,3 +136,4 @@ export default function ServicePage({ params }: { params: { slug: string } }) {
 
 
 
+
--- src/app/zone/[slug]/page.tsx
+++ src/app/zone/[slug]/page.tsx
@@ -127,3 +127,4 @@ export default function ZonePage({ params }: { params: { slug: string } }) {
 
 
 
+
